655bdd8a9b37b5b19f614711ec97aac451df71a1
rashadat1 <rashadat@yahoo.com>
2025-03-09 00:06:08 -0500

    feature: parentnode implementation and test

---
+++ src/parentnode.py
@@ -0,0 +1,15 @@
+from typing import Dict, List, Optional
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children: List[HTMLNode], props: Optional[Dict[str,str]] = None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if not self.children:
+            # base case we reach a leaf node and call its to_html method
+            return self.to_html()
+        return_string = f"<{self.tag}" + f"{self.props_to_html()}>"
+        for child in self.children:
+            return_string += child.to_html()
+        return_string += f"</{self.tag}>"
