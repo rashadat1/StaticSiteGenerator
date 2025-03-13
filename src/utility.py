8c870e7b48a3bfc3f6bd91b76e9d60dfd594bd12
rashadat1 <rashadat@yahoo.com>
2025-03-13 09:05:23 -0400

    fixed utilities and completed text -> text_node

--- src/utility.py
+++ src/utility.py
@@ -80,41 +80,47 @@ def append_nodes_helper(lst1: List[TextNode], lst2: List[TextNode]) -> List[Text
 def split_nodes_image(old_nodes: List[TextNode]) -> List[TextNode]:
     result = []
     def helper(node: TextNode) -> List[TextNode]:
-        intermediate = node.text
-        image_parts = extract_markdown_images(node.text)
-        for i in range(len(image_parts)):
-            intermediate = ("seqbreakabc".join(intermediate.split(f"![{image_parts[i][0]}]({image_parts[i][1]})")))
-        non_image_parts = intermediate.split("seqbreakabc")
-        
-        non_image_text_nodes = [TextNode(text=text_frag, text_type=TextType.TEXT) for text_frag in non_image_parts]
-        image_text_nodes = [TextNode(text=altText, text_type=TextType.IMAGE, url=urlText) for altText, urlText in image_parts]
-        
-        if node.text.startswith(non_image_parts[0]):
-            lst1 = non_image_text_nodes
-            lst2 = image_text_nodes
-        else:
-            lst1 = image_text_nodes
-            lst2 = non_image_text_nodes
-        result = append_nodes_helper(lst1, lst2)
+        result = []
+        if node.text_type == TextType.TEXT:
+            intermediate = node.text
+            image_parts = extract_markdown_images(node.text)
+            for i in range(len(image_parts)):
+                intermediate = ("seqbreakabc".join(intermediate.split(f"![{image_parts[i][0]}]({image_parts[i][1]})")))
+            non_image_parts = intermediate.split("seqbreakabc")
+            
+            non_image_text_nodes = [TextNode(text=text_frag, text_type=TextType.TEXT) for text_frag in non_image_parts]
+            image_text_nodes = [TextNode(text=altText, text_type=TextType.IMAGE, url=urlText) for altText, urlText in image_parts]
+            if node.text.startswith(non_image_parts[0]):
+                lst1 = non_image_text_nodes
+                lst2 = image_text_nodes
+            else:
+                lst1 = image_text_nodes
+                lst2 = non_image_text_nodes
+            result = append_nodes_helper(lst1, lst2)
         return result
      
     for node in old_nodes:
-        result.extend(helper(node))
+        if node.text_type == TextType.TEXT:
+            result.extend(helper(node))
+        else:
+            result.append(node)
+
     
     return result
 
 def split_nodes_link(old_nodes: List[TextNode]) -> List[TextNode]:
     result = []
     def helper(node: TextNode) -> List[TextNode]:
+        result = []
         intermediate = node.text
         link_parts = extract_markdown_links(node.text)
         for i in range(len(link_parts)):
             intermediate = ("seqbreakabc".join(intermediate.split(f"[{link_parts[i][0]}]({link_parts[i][1]})")))
         non_link_parts = intermediate.split("seqbreakabc")
-        
-        non_link_text_nodes = [TextNode(text=text_frag, text_type=TextType.TEXT) for text_frag in non_link_parts]
+    
+        non_link_text_nodes = [TextNode(text=text_frag, text_type=node.text_type) for text_frag in non_link_parts]
         link_text_nodes = [TextNode(text=altText, text_type=TextType.LINK, url=urlText) for altText, urlText in link_parts]
-        
+    
         if node.text.startswith(non_link_parts[0]):
             lst1 = non_link_text_nodes
             lst2 = link_text_nodes
@@ -125,10 +131,22 @@ def split_nodes_link(old_nodes: List[TextNode]) -> List[TextNode]:
         return result
      
     for node in old_nodes:
-        result.extend(helper(node))
+        if node.text_type == TextType.TEXT:
+            result.extend(helper(node))
+        else:
+            result.append(node)
     
     return result 
 
+def text_to_textnodes(text: str) -> List[TextNode]:
+    textNode = TextNode(text=text, text_type=TextType.TEXT)
+    result = []
+    result.extend(split_nodes_link(split_nodes_image([textNode])))
+    delimiters = ["`","**","_"]
+    textTypes = [TextType.CODE, TextType.BOLD, TextType.ITALIC]
+    for delimiter, textType in zip(delimiters, textTypes):
+        result = split_nodes_delimiter(result, delimiter, textType)
+    return result
 
 
 if __name__ == "__main__":
@@ -137,5 +155,16 @@ if __name__ == "__main__":
     TextType.TEXT)
 
     new_nodes = split_nodes_link([node])
-    print(new_nodes)
+    #print(new_nodes)
+
+    node2 = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev), ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg))",
+    TextType.TEXT)
+
+    #new_nodes = split_nodes_image([node2])
+    #print(new_nodes)
+
+    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    
+    print(text_to_textnodes(text))
 
